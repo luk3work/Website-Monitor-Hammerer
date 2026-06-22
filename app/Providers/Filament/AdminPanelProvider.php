5af9639d7a5bd605e737fea199b25323fd101e10
Lukas Wagner <wagner@hammerer.at>
2026-06-22 14:19:30 +0000

    Feature: Benutzerverwaltung (Resource, Admin-only, Rollen) + Nav-Gruppe Verwaltung

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,7 @@ class AdminPanelProvider extends PanelProvider
             // Cockpit-Charakter: volle Breite, einklappbare Sidebar, gruppierte Navigation, SPA.
             ->maxContentWidth(MaxWidth::Full)
             ->sidebarCollapsibleOnDesktop()
-            ->navigationGroups(['Betrieb', 'Stammdaten'])
+            ->navigationGroups(['Betrieb', 'Stammdaten', 'Verwaltung'])
             ->spa()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
